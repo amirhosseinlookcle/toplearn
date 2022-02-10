79c03a5871e12023c78a7053495204cad65fdf9e
Amir <amirhosseinlookcle@gmail.com>
2022-02-10 17:00:11 +0330

    register created and added

--- src/containers/Toplearn.jsx
+++ src/containers/Toplearn.jsx
@@ -3,6 +3,7 @@ import Course from '../components/Courses/Course';
 import {Route, Routes} from 'react-router-dom';
 import MainLayout from '../components/layouts/MainLayout';
 import Login from '../components/Login/Login';
+import Register from '../components/register/Register';
 
 const Toplearn = (props) => {
     return (
@@ -10,6 +11,7 @@ const Toplearn = (props) => {
             <Routes>
                 <Route path='/login'  element={<Login/>}/>
                 <Route path='/' element={<Course/>}/>
+                <Route path='/register' element={<Register/>}/>
             </Routes>
         </MainLayout>
 
